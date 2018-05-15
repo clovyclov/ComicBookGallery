56057370e964be4be3fc9b0b97c0d0fd5bbc03c3
Clovis Shropshire <shropshireclovis@gmail.com>
2018-05-14 20:54:24 -0400

    Added ComicBook Detail view

---
+++ Backup/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        public ContentResult Detail()
+        {
+            if(DateTime.Today.DayOfWeek == DayOfWeek.Monday){
+                return Content($"Today is {DayOfWeek.Monday.ToString()}");
+            }
+            return Content("HEY");
+        }
+    }
+}
